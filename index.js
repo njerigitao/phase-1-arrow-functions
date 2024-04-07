19eb177fe43732ab8c097ab9e229113c796dda46
AGNESGITAU <agnesgitau40@gmail.com>
2024-04-07 10:39:12 +0300

    arrow-functions

---
+++ index.js
@@ -0,0 +1,9 @@
+//Define function expression called divide
+function divide(){
+    return 2000/100;
+}
+//Define arrow function called square
+const square = (num)=> num*num;
+
+//Define arrow function called add
+const add = (num1,num2)=> num1+num2;
